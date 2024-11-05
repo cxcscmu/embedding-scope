bd9fcf697b5cb4b947e9daabc5777bfc76f95560
haok1402 <haok1402@gmail.com>
2024-11-05 10:48:01 -0500

    cleanup: remove unnecessary files

--- main.py
+++
@@ -1,7 +0,0 @@
-from source.dataset.msMarco import MsMarco
-from source.embedding.miniCPM import MiniCPM
-
-dataset = MsMarco()
-passageID, passageEmbedding = next(dataset.getPassageEmbeddings(MiniCPM))
-print(passageID, type(passageID))
-print(passageEmbedding)
