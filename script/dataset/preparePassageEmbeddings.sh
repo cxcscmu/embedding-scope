b242eaab8aa27b899065252b603aa4f5fa9098dc
haok1402 <haok1402@gmail.com>
2024-11-08 09:26:30 -0500

    cleanup: prepare passage embedding scripts

--- script/dataset/preparePassageEmbeddings.sh
+++ script/dataset/preparePassageEmbeddings.sh
@@ -10,20 +10,20 @@
 #SBATCH --exclude=babel-1-31,babel-0-37
 #SBATCH --array=0-7
 
-##############################################################################
+####################################################################
 # Load the required modules.
-##############################################################################
+####################################################################
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate scope
 
-##############################################################################
+####################################################################
 # Prepare the MS MARCO dataset.
-##############################################################################
+####################################################################
 
-# ENTRYPOINT="source.dataset.textRetrieval.msMarco"
-# SHAREDCMDS="--embedding miniCPM --gpuDevice 0 --batchSize 128 --numShards 256 --workerCnt $SLURM_ARRAY_TASK_COUNT"
-# python3 -m $ENTRYPOINT preparePassageEmbeddings $SHAREDCMDS --workerIdx $SLURM_ARRAY_TASK_ID
+ENTRYPOINT="source.dataset.textRetrieval.msMarco"
+SHAREDCMDS="--embedding miniCPM --gpuDevice 0 --batchSize 128 --numShards 256 --workerCnt $SLURM_ARRAY_TASK_COUNT"
+python3 -m $ENTRYPOINT preparePassageEmbeddings $SHAREDCMDS --workerIdx $SLURM_ARRAY_TASK_ID
 
 ENTRYPOINT="source.dataset.textRetrieval.msMarco"
 SHAREDCMDS="--embedding bgeBase --gpuDevice 0 --batchSize 512 --numShards 64 --workerCnt $SLURM_ARRAY_TASK_COUNT"
