6270b7192cf8e84825eeb49e69a5e45fea91689f
haok1402 <haok1402@gmail.com>
2024-11-11 13:24:01 -0500

    verify that the pipeline is correct

--- script/dataset/prepareQueryEmbeddings.sh
+++ script/dataset/prepareQueryEmbeddings.sh
@@ -22,13 +22,13 @@ conda activate scope
 ####################################################################
 
 ENTRYPOINT="source.dataset.textRetrieval.msMarco"
-SHAREDCMDS="--embedding miniCPM --gpuDevice 0 --batchSize 128 --workerCnt $SLURM_ARRAY_TASK_COUNT"
-python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 16 --partition "train" --workerIdx $SLURM_ARRAY_TASK_ID
+SHAREDCMDS="--embedding miniCPM --gpuDevice 0 --batchSize 512 --workerCnt $SLURM_ARRAY_TASK_COUNT"
+python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 14 --partition "train" --workerIdx $SLURM_ARRAY_TASK_ID
 python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 2 --partition "dev" --workerIdx $SLURM_ARRAY_TASK_ID
 python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 2 --partition "eval" --workerIdx $SLURM_ARRAY_TASK_ID
 
-ENTRYPOINT="source.dataset.textRetrieval.msMarco"
-SHAREDCMDS="--embedding bgeBase --gpuDevice 0 --batchSize 512 --workerCnt $SLURM_ARRAY_TASK_COUNT"
-python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 8 --partition "train" --workerIdx $SLURM_ARRAY_TASK_ID
-python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 1 --partition "dev" --workerIdx $SLURM_ARRAY_TASK_ID
-python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 1 --partition "eval" --workerIdx $SLURM_ARRAY_TASK_ID
+# ENTRYPOINT="source.dataset.textRetrieval.msMarco"
+# SHAREDCMDS="--embedding bgeBase --gpuDevice 0 --batchSize 512 --workerCnt $SLURM_ARRAY_TASK_COUNT"
+# python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 8 --partition "train" --workerIdx $SLURM_ARRAY_TASK_ID
+# python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 1 --partition "dev" --workerIdx $SLURM_ARRAY_TASK_ID
+# python3 -m $ENTRYPOINT prepareQueryEmbeddings $SHAREDCMDS --numShards 1 --partition "eval" --workerIdx $SLURM_ARRAY_TASK_ID
--- script/dataset/prepareQueryNeighbors.sh
+++ script/dataset/prepareQueryNeighbors.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=general
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:A6000:4
+#SBATCH --gres=gpu:A6000:2
 #SBATCH --mem=256G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=haok@andrew.cmu.edu
@@ -21,7 +21,13 @@ conda activate scope
 ##############################################################################
 
 ENTRYPOINT="source.dataset.textRetrieval.msMarco"
-SHAREDCMDS="--embedding miniCPM --gpuDevice 0 1 2 3 --batchSize 1024 --topK 128"
-python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition train
+SHAREDCMDS="--embedding miniCPM --gpuDevice 0 1 --batchSize 2048 --topK 128"
+# python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition train
 python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition dev
 python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition eval
+
+# ENTRYPOINT="source.dataset.textRetrieval.msMarco"
+# SHAREDCMDS="--embedding bgeBase --gpuDevice 0 1 --batchSize 2048 --topK 128"
+# python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition train
+# python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition dev
+# python3 -m $ENTRYPOINT prepareQueryNeighbors $SHAREDCMDS --partition eval
